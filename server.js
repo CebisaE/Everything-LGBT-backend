031c60cd11657ce47e9283af476547989acfc56a
emihle <mihlekcebisa@gmail.com>
2022-03-17 10:28:36 +0200

    fixedport

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const cors = require("cors")
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://emihle:Cebisa02@everythinglgbtplus.fcazp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
 
+app.set('port',process.env.PORT || 3000);
 app.use(express.json())
 app.use(cors())
 
@@ -15,7 +17,7 @@ const productsRouter = require('./app/routes/products.routes')
 const customerRouter = require('./app/routes/customer.routes')
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 const cartRouter = require("./app/routes/cart.routes");
 app.use('/products', productsRouter)
 app.use('/customer', customerRouter)
@@ -24,6 +26,6 @@ app.use("/cart", cartRouter);
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
+app.listen(app.get("port"), () => {
     console.log(`Server started on port ${port}`)
 });
