206c935bf9e805227de96f8e32a65fe280c64915
emihle <mihlekcebisa@gmail.com>
2022-03-26 13:58:28 +0200

    whitespace

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ app.set('port',process.env.PORT || 3000);
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-// const db = mongoose.connection
-// db.on('error', (error) => console.log(error))
-// db.once('open', () => console.log('Connected to Database'))
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
+
 
 function initial() {
     Role.estimatedDocumentCount((err, count) => {
